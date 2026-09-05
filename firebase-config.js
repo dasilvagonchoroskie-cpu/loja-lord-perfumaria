3a3d5e01740bfdbf4e42cdfbe0abd2301f3ecbbf
dasilvagonchoroskie-cpu <dasilvagonchoroskie@gmail.com>
2026-09-05 09:09:28 -0300

    Atualizar o firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,13 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCuMcwoWZcTKavvYd1HCPsGYWLWQoFmXqY",
-  authDomain: "lord-perfumaria-156f1.firebaseapp.com",
-  projectId: "lord-perfumaria-156f1",
-  storageBucket: "lord-perfumaria-156f1.firebasestorage.app",
-  messagingSenderId: "788098923932",
-  appId: "1:788098923932:web:9fb840866c38f987839c59"
+  apiKey: "AIzaSyAGuOxDpye_kiyHxutNRRNs3Drrv46Bdb4",
+  authDomain: "lord-perfumaria-51e77.firebaseapp.com",
+  projectId: "lord-perfumaria-51e77",
+  storageBucket: "lord-perfumaria-51e77.firebasestorage.app",
+  messagingSenderId: "737028905393",
+  appId: "1:737028905393:web:552a4989923ff06dda0590"
 };
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 const auth = firebase.auth();
+
